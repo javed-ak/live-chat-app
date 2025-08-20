eed90bf631cf8f8c600e61be6eb78559feed0ff6
javed-ak <javedakhtarsofficial@gmail.com>
2025-08-20 22:15:20 +0530

    init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: 'dist', // <-- Important for Vercel
-  },
+  }
 })
